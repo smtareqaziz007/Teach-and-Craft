f3426c89a3559528568e89a12befd534db261475
S M Tareq Aziz <smtareqaziz007@gmail.com>
2024-03-27 01:10:29 +0600

    Search Bar added

--- src/pages/Students.js
+++ src/pages/Students.js
@@ -4,6 +4,10 @@ import Container from "react-bootstrap/Container";
 import ListGroup from "react-bootstrap/ListGroup";
 import ListHeader from "../components/ListHeader";
 import ListItem from "../components/ListItem";
+import Button from "react-bootstrap/Button";
+import InputGroup from "react-bootstrap/InputGroup";
+import FormControl from "react-bootstrap/FormControl";
+import { BsPlus } from "react-icons/bs";
 
 const Students = () => {
   const [studentData, setStudentData] = useState(data);
@@ -19,19 +23,39 @@ const Students = () => {
   };
 
   return (
-    <Container style={{ maxWidth: "900px" }}>
-      <ListGroup>
-        <ListHeader />
-        {studentData.map((student) => (
-          <ListItem
-            key={student.id}
-            student={student}
-            handleEdit={handleEdit}
-            handleRemove={handleRemove}
+    <>
+      <Container
+        style={{ maxWidth: "900px", marginBottom: "20px", marginTop: "20px" }}
+      >
+        <InputGroup>
+          <Button variant="dark" style={{ borderRadius: 20 }}>
+            <BsPlus size={30} />
+          </Button>
+          <FormControl
+            placeholder="Search..."
+            style={{
+              marginLeft: "10px",
+              borderWidth: "2px",
+              borderColor: "black",
+              borderRadius: 20,
+            }}
           />
-        ))}
-      </ListGroup>
-    </Container>
+        </InputGroup>
+      </Container>
+      <Container style={{ maxWidth: "900px" }}>
+        <ListGroup>
+          <ListHeader />
+          {studentData.map((student) => (
+            <ListItem
+              key={student.id}
+              student={student}
+              handleEdit={handleEdit}
+              handleRemove={handleRemove}
+            />
+          ))}
+        </ListGroup>
+      </Container>
+    </>
   );
 };
 
